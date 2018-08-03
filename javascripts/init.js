e8ea00ccedebe09aa9779cf946610fb32537a5dd
vagmi <vagmi.mudumbai@gmail.com>
2018-08-03 10:20:17 +0530

    add serverless forms endpoint

--- javascripts/init.js
+++ javascripts/init.js
@@ -1,1 +1,14 @@
 particlesJS.load('particles', 'particlesjs-config.json');
+
+let formElem = document.querySelector("#form-container form");
+formElem.addEventListener("submit",(e) => {
+    e.preventDefault();
+    fetch(formElem.action,{
+        method: "POST",
+        mode: "no-cors",
+        body: new FormData(formElem)
+    }).then(()=> {
+        let fc=document.getElementById("form-container");
+        fc.innerHTML='<p class="thanks">Thanks for your interest. We will get in touch with you shortly</p>';
+    })
+});
